02e83367d4a729d06feeda78375d3e9cc2d79a7e
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2020-02-19 11:28:51 +0100

    refactor: export all members properly

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 "use strict";
 
-// Require Third-party Dependencies
-const astring = require("astring");
-
-// Require Internal
-const { Expression, Block, Debugger } = require("./ESTree/Statements");
-const { CallExpression, AutomaticMemberExpression, ArrowFunctionExpression } = require("./ESTree/Expression");
-const VariableDeclaration = require("./ESTree/VariableDeclaration");
-const VariableDeclarator = require("./ESTree/VariableDeclarator");
+const Expression = require("./ESTree/Expression");
+const FunctionDeclaration = require("./ESTree/FunctionDeclaration");
 const Identifier = require("./ESTree/Identifier");
 const Literal = require("./ESTree/Literal");
+const Modules = require("./ESTree/Modules");
 const Program = require("./ESTree/Program");
+const Property = require("./ESTree/Property");
+const Statements = require("./ESTree/Statements");
+const SwitchStatement = require("./ESTree/SwitchStatement");
+const VariableDeclaration = require("./ESTree/VariableDeclaration");
+const VariableDeclarator = require("./ESTree/VariableDeclarator");
+const Utils = require("./ESTree/Utils");
 
-const ConsoleLog = AutomaticMemberExpression("console", "log");
-const Event = AutomaticMemberExpression("foo", "on");
-const log = (message) => CallExpression(ConsoleLog, [new Literal(message).toJSON()]);
-const requireSample = (depName) => CallExpression(AutomaticMemberExpression("require"), [new Literal(depName).toJSON()]);
-
-const AST = new Program();
-AST.add(VariableDeclaration.createOne("const", "http", requireSample("http")));
-AST.add(CallExpression(Event, [new Literal("start").toJSON(), ArrowFunctionExpression(Block())]));
-
-console.log(JSON.stringify(AST.body, null, 4));
-console.log(astring.generate(AST.toJSON()));
-
+module.exports = {
+    Expression,
+    FunctionDeclaration,
+    Identifier,
+    Literal,
+    Modules,
+    Program,
+    Property,
+    Statements,
+    SwitchStatement,
+    VariableDeclaration,
+    VariableDeclarator,
+    Utils
+};
