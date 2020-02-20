762e33d016f17510f9a36ef5766b33adeb915312
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2020-02-20 14:46:42 +0100

    feat: add es2015 template

--- ESTree/Template.js
+++ ESTree/Template.js
@@ -1,5 +1,29 @@
 "use strict";
 
-module.exports = {
+function TemplateLiteral(quasis = [], expressions = []) {
+    return {
+        type: "TemplateLiteral",
+        quasis,
+        expressions
+    };
+}
 
+function TaggedTemplateExpression(tag, quasi) {
+    return {
+        type: "TaggedTemplateExpression", tag, quasi
+    };
+}
+
+function TemplateElement(tail = false, cooked, raw = cooked) {
+    return {
+        type: "TemplateElement",
+        tail,
+        value: { raw, cooked }
+    };
+}
+
+module.exports = {
+    TemplateLiteral,
+    TaggedTemplateExpression,
+    TemplateElement
 };
